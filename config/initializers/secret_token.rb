141e7b159ec1fb48fa140c33f3db289ecca595d3
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-07-08 16:35:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsProject::Application.config.secret_token = '10bcebdd99515d5ff1beb576d1322b95fcab8e6982262145684496d0283d2916de912a543727ee9e2e39747247a8f62e9258e5534a35909748c6151b83831437'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+RailsProject::Application.routes.draw do
+  root :to => 'static_pages#index'
+end
